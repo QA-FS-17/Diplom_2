9e2e22d440ab8e4f8626590ff5b7092f3c79ca44
Vyacheslav Rudenko <globalnet2078@gmail.com>
2025-05-10 13:58:15 +0300

    Modified helpers\api_client.py

--- helpers/api_client.py
+++ helpers/api_client.py
@@ -17,6 +17,7 @@ class StellarBurgersApi:
 
     # User methods
     def create_user(self, email, password, name):
+        """Регистрация нового пользователя"""
         endpoint = "auth/register"
         payload = {
             "email": email,
@@ -26,6 +27,7 @@ class StellarBurgersApi:
         return self._request("POST", endpoint, json=payload)
 
     def login_user(self, email, password):
+        """Авторизация пользователя"""
         endpoint = "auth/login"
         payload = {
             "email": email,
@@ -34,12 +36,14 @@ class StellarBurgersApi:
         return self._request("POST", endpoint, json=payload)
 
     def delete_user(self, access_token):
+        """Удаление пользователя"""
         endpoint = "auth/user"
         headers = {"Authorization": access_token}
         return self._request("DELETE", endpoint, headers=headers)
 
     # Order methods
     def create_order(self, ingredients, access_token=None):
+        """Создание заказа"""
         endpoint = "orders"
         headers = {}
         if access_token:
@@ -47,15 +51,19 @@ class StellarBurgersApi:
         payload = {"ingredients": ingredients}
         return self._request("POST", endpoint, json=payload, headers=headers)
 
-    def get_user_orders(self, access_token):
-        endpoint = "orders"
-        headers = {"Authorization": access_token}
-        return self._request("GET", endpoint, headers=headers)
+    def get_user_orders(self, access_token=None):
+        """Получение заказов пользователя"""
+        headers = {}
+        if access_token:
+            headers["Authorization"] = access_token
+        return self._request("GET", "orders", headers=headers)
 
     # Profile methods
-    def update_user_info(self, access_token, email=None, password=None, name=None):
-        endpoint = "auth/user"
-        headers = {"Authorization": access_token}
+    def update_user_info(self, access_token=None, email=None, password=None, name=None):
+        """Обновление данных пользователя"""
+        headers = {}
+        if access_token:
+            headers["Authorization"] = access_token
         payload = {}
         if email:
             payload["email"] = email
@@ -63,9 +71,10 @@ class StellarBurgersApi:
             payload["password"] = password
         if name:
             payload["name"] = name
-        return self._request("PATCH", endpoint, json=payload, headers=headers)
+        return self._request("PATCH", "auth/user", json=payload, headers=headers)
 
     def get_user_info(self, access_token):
+        """Получение информации о пользователе"""
         endpoint = "auth/user"
         headers = {"Authorization": access_token}
         return self._request("GET", endpoint, headers=headers)
